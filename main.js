e114dbbefac11a306f457637b97c45c2ebe00b89
Eduardo Morelato <dudumorelato1@gmail.com>
2026-04-02 00:57:35 -0300

    nova animação js

---
+++ main.js
@@ -0,0 +1,27 @@
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".slice", {
+    scrollTrigger: {
+        trigger: ".sec1", 
+        start: "top top", 
+        end: "+=150%",    
+        scrub: 1,         
+        pin: true,        
+    },
+    yPercent: 100, 
+    stagger: 0.1,  
+    ease: "none"
+});
+
+// REVELANDO A FILOSOFIA DEPOIS DA TELA PRETA
+gsap.from(".sec2 .filosofia", {
+    scrollTrigger: {
+        trigger: ".sec2",
+        start: "top 70%", 
+    },
+    y: 50, 
+    opacity: 0, 
+    duration: 1.2,
+    ease: "power3.out"
+});
